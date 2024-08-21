287c6faebe2244170a3c9e7de2e84440f57305ac
Blaj3n <benceulrich@gmail.com>
2024-08-21 10:22:28 +0200

    szavak belvasása és ellenőrzése

---
+++ szolanc.py
@@ -0,0 +1,20 @@
+szolanc = []
+szamlalo = 0
+
+while True:
+    szo = input(f"{szamlalo + 1}. szó: ")
+    if szamlalo == 0:
+        if len(szo) != 6:   # Első szó esetén nincs ellenőrzés
+            print("A karakterek száma téves! ")
+            break
+    else:
+        # Minden további szó esetén ellenőrizni kell a hosszt és az illeszkedést
+        if len(szo) != 6:
+            print("A karakterek száma téves! ")
+            break
+        if szo[0] != szolanc[-1][-1]:   # Az első betű nem egyezik az előző szó utolsó betűjével
+            print("Nem illeszkedett!")
+            break
+
+    szolanc.append(szo) # Ha megfelel, hozzáadjuk a szavak listájához
+    szamlalo += 1
